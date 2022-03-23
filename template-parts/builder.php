ca00eeb6e199a08c58453868a79a441bf3475eb1
mazurkov2010@gmail.ocm <As6152as>
2022-03-23 15:23:21 +0200

    this 3 files where I have done max changes

--- template-parts/builder.php
+++ template-parts/builder.php
@@ -113,6 +113,7 @@
 					<?php get_template_part('template-parts/sections/section-case-study-content'); ?>
 				<?php } ?>
 
+
                 <?php if ($section == 'section-cases-pricing') { ?>
                     <?php get_template_part('template-parts/sections/section-cases-pricing'); ?>
                 <?php } ?>
